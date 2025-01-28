84534c4b9744d50edf41b4242b22d780281ba68b
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-01-27 22:09:15 -0600

    working toward fixing error where it only shows the shelby park for view location details and edit location details

--- src/api/mergedData.js
+++ src/api/mergedData.js
@@ -1,9 +1,9 @@
-import { getLocationTours, getSingleLocationByPK } from './locationData';
+import { getLocationTours, getSingleLocationById } from './locationData';
 
 // below is a little different from our wireframe because it gives us access to the tours at the location. keeping for now in case we might want to show the tours offered at a location in a location's details page
 const viewLocationDetails = (LocationId) =>
   new Promise((resolve, reject) => {
-    Promise.all([getSingleLocationByPK(LocationId), getLocationTours(LocationId)])
+    Promise.all([getSingleLocationById(LocationId), getLocationTours(LocationId)])
       .then(([locationObject, locationToursArray]) => {
         resolve({ ...locationObject.fields, tours: locationToursArray });
       })
