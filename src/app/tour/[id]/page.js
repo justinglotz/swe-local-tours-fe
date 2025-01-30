8fcba0cdcbe019b2f75fab1becc369122a66d043
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-01-30 07:49:56 -0600

    added back-to-tours button

--- src/app/tour/[id]/page.js
+++ src/app/tour/[id]/page.js
@@ -1,7 +1,5 @@
 'use client';
 
-// view tour details here
-// put JSX here directly, no other component used in here
 import React, { useEffect, useState } from 'react';
 import { getSingleTour } from '@/api/tourData';
 import dayjs from 'dayjs';
@@ -10,6 +8,7 @@ import { getSingleLocation } from '@/api/locationData';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Card } from 'react-bootstrap';
+import Link from 'next/link';
 
 export default function TourDetailsPage({ params }) {
   const [tour, setTour] = useState({});
@@ -21,51 +20,65 @@ export default function TourDetailsPage({ params }) {
         setTour({ ...data, locationName: locationData.name, locationAddress: locationData.address });
       });
     });
-  });
+  }, []);
 
   const tourDateObj = dayjs(tour.date).format('ddd, MMM D YYYY');
   const tourTimeObj = dayjs(`2000-01-01 ${tour.time}`).format('h:mm A');
 
   return (
-    <div className="flex flex-wrap flex-col">
-      <Row>
-        <Col className="col-4">
-          <div className="h-[33vh]">
-            <img src={tour.image} alt={tour.name} className="w-full h-full object-cover" />
-          </div>
-        </Col>
-        <Col className="col-4" />
-        <Col className="col-4 d-flex flex-column justify-content-end">
-          <button type="button" className="btn btn-primary">
-            Add to Itinerary
-          </button>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="col-4">
-          <div className="h-[66vh]">
-            <div>
-              <h1 className="text-center text-2xl">{tour.name}</h1>
+    <div className="flex flex-col h-[calc(100vh-60px)] overflow-hidden">
+      {/* Top section - 33vh */}
+      <div className="h-1/3">
+        <Row className="h-full">
+          <Col className="col-4 h-full">
+            <div className="h-full">
+              <img src={tour.image} alt={tour.name} className="w-full h-full object-cover" />
             </div>
-            <p className="text-center">Date: {tourDateObj}</p>
-            <p className="text-center">Time: {tourTimeObj}</p>
-          </div>
-        </Col>
-        <Col className="col-8">
-          <Card className="h-100" bg="dark" text="white">
-            <Card.Body>
-              <Card.Title className="text-6xl">TOUR DETAILS</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-              <Card.Text>
-                <p>Location: {tour.locationName}</p>
-                <p>Address: {tour.locationAddress}</p>
-                <p>Duration: {tour.duration} minutes</p>
-                <p>Price: ${tour.price}</p>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col className="col-4" />
+          <Col className="col-4 d-flex flex-col justify-end pb-4">
+            <button type="button" className="btn btn-primary">
+              Add to Itinerary
+            </button>
+          </Col>
+        </Row>
+      </div>
+
+      {/* Bottom section - 66vh */}
+      <div className="h-2/3">
+        <Row className="h-full">
+          <Col className="col-4 d-flex flex-col">
+            <div className="flex-grow">
+              <div>
+                <h1 className="text-center text-2xl">{tour.name}</h1>
+              </div>
+              <p className="text-center">Date: {tourDateObj}</p>
+              <p className="text-center">Time: {tourTimeObj}</p>
+            </div>
+            <div className="mt-auto pb-4">
+              <Link href="/tours" passHref>
+                <button className="btn btn-info" type="button">
+                  Back to Tours
+                </button>
+              </Link>
+            </div>
+          </Col>
+          <Col className="col-8 h-full">
+            <Card className="h-full" bg="dark" text="white">
+              <Card.Body className="d-flex flex-col">
+                <Card.Title className="text-6xl">TOUR DETAILS</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Text className="flex-grow">
+                  <p>Location: {tour.locationName}</p>
+                  <p>Address: {tour.locationAddress}</p>
+                  <p>Duration: {tour.duration} minutes</p>
+                  <p>Price: ${tour.price}</p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
