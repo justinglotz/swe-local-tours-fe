298d888d9feedfbd1081aa61c823ab48332b9a35
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-02-02 22:38:48 -0600

    form displays even though getSingleUer is broken

--- src/app/page.js
+++ src/app/page.js
@@ -15,17 +15,22 @@ function Home() {
   const router = useRouter();
 
   // define userObj in state. it is updated by ProfileForm
-  const [userObj, setUserObj] = useState(null); // set initial state to null
+  const [userObj, setUserObj] = useState({
+    first_name: '',
+    last_name: '',
+    bio: '',
+    uid: '',
+  }); // set initial state to user object with empty strings
 
   // useEffect to get user object from the database, the dependecy array should be userObj (from useState), so it runs the effect when userObj changes. the callback function should be:
   useEffect(() => {
     if (user) {
+      // if user is defined
       getSingleUser(user.uid)
         .then((data) => {
           // TODO: getSingleUser not currently working correctly. need to fix on BE
           setUserObj(data);
-          setUserObj(data);
-
+          console.log('userObj:', userObj);
           // if user object bio is not an empty string, route to tours page
           if (data?.bio !== '') {
             router.push('/tours');
@@ -37,8 +42,25 @@ function Home() {
     }
   }, [user]);
 
-  // default: display the ProfileForm component.
+  // if userObj is available and bio is not empty, redirect to tours page
+  if (userObj?.bio !== '') {
+    // TODO: Make sure this works once getSingleUser is fixed
+    return (
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <p>Redirecting...</p>
+      </div>
+    );
+  }
 
+  // if userObj is available and bio is empty, display the ProfileForm component
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -49,14 +71,8 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      {userObj.bio ? (
-        <p>Redirecting...</p>
-      ) : (
-        <>
-          <h1>Hello {user.displayName}! </h1>
-          <ProfileForm />
-        </>
-      )}
+      <h1>Hello {user.displayName}!</h1>
+      <ProfileForm />
     </div>
   );
 }
